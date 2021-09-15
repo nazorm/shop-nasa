2fb7a184329d54fa081373d7bbf19f63aa8aa0b0
Chinazor Nnadozie <nnadoziechinazor@gmail.com>
2021-09-15 14:48:31 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { useQuery } from 'react-query';
 import axios from 'axios';
@@ -7,14 +7,29 @@ import NasaCard from './NasaCard';
 function App() {
   const currentFetchedPage: number = 1
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [scrolled, setScrolled] = useState<boolean>(false)
 
-
-// fetch Nasa data
+  // fetch Nasa data
   const fetchData = async (currentFetchedPage: number) => {
     const { data } = await axios.get(`http://images-api.nasa.gov/search?page=${currentFetchedPage}&q=apollo+11&description=moon+landing&media_type=image`)
     return data
   }
 
+
+
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  })
+
+  // handle scroll
+  const handleScroll = () => {
+    if (window.pageYOffset > 0) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
+  }
 
   //const { status, data, error, isPreviousData } = useQuery(
   const { status, data, error } = useQuery(
@@ -24,13 +39,12 @@ function App() {
 
   //number of posts displayed per page
   const postsPerPage: number = 20;
-// console.log(data.collection.items[0].nasa_id)
-// console.log(data?.collection.items[0].data[0].nasa_id)
+
 
   //get number of pages
   //number of entire post / number of posts per page
   const totalPost = data?.collection.items.length
-  const pageCount: number = totalPost/postsPerPage;
+  const pageCount: number = totalPost / postsPerPage;
 
 
   //get current posts
@@ -42,44 +56,66 @@ function App() {
   if (error) return <p>Error...</p>
 
   return (
-    <div className="App">
-      {status === 'loading' ? 'loading...' :
-        <div className='nasacard-container'>
-          {currentPosts.map((nasaData: any,) => {
-            return (
-              <NasaCard
-                key={nasaData.data[0].nasa_id}
-                data={nasaData.data}
-                links={nasaData.links}
+    <div className="app">
 
-              />
-            )
-          })}
-        </div>}
+      <header className={scrolled ? ' header scrolled-header' : 'header static-header'} style={{ position: 'fixed' }}>
+        <h1>Shop Nasa</h1>
+        <nav>
+          <ul>
+            <li>
+              Merch
+            </li>
 
-      {status === 'loading' ? '' : (
-        <div className='pagination'>
-          <div className='pages'>
-            <button className={currentPage === 1 ? 'disabled' : ''}
-              onClick={() => setCurrentPage(old => Math.max(old - 1, 1))}
-              disabled={currentPage === 1}
-            >
-              Previous
-            </button>
-            <p> {currentPage}/{pageCount}</p>
-            <button className={currentPage === pageCount ? 'disabled' : ''}
-              onClick={() => {
-                setCurrentPage(old => (pageCount >= 1 ? old + 1 : old))
-              }}
-              disabled={currentPage === pageCount}
-            >
-              Next
-            </button>
+            <li>
+              Photos
+            </li>
+            <li>
+              Vouchers
+            </li>
+
+          </ul>
+        </nav>
+      </header>
+
+      <div>
+        {status === 'loading' ? 'loading...' :
+          <div className='nasacard-container'>
+            {currentPosts.map((nasaData: any,) => {
+              return (
+                <NasaCard
+                  key={nasaData.data[0].nasa_id}
+                  data={nasaData.data}
+                  links={nasaData.links}
+
+                />
+              )
+            })}
+          </div>}
+
+        {status === 'loading' ? '' : (
+          <div className='pagination'>
+            <div className='pages'>
+              <button className={currentPage === 1 ? 'disabled' : 'page-btn'}
+                onClick={() => setCurrentPage(old => Math.max(old - 1, 1))}
+                disabled={currentPage === 1}
+              >
+                Previous
+              </button>
+              <p> {currentPage}/{pageCount}</p>
+              <button className={currentPage === pageCount ? 'disabled' : 'page-btn'}
+                onClick={() => {
+                  setCurrentPage(old => (pageCount >= 1 ? old + 1 : old))
+                }}
+                disabled={currentPage === pageCount}
+              >
+                Next
+              </button>
+            </div>
+
+
           </div>
-
-
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
--- src/NasaCard.tsx
+++ src/NasaCard.tsx
@@ -3,55 +3,53 @@ import redHeartShape from './assets/redheart-shape.svg';
 import heartShape from './assets/heart-shape.svg';
 
 export interface NasaCardProps {
-    data: IData[],
-    links: ILinks[],
+  data: IData[],
+  links: ILinks[],
 }
 interface IData {
-    date_created: string,
-    description: string,
-    location: string,
-    title: string,
-    photographer: string
-    nasa_id:number
+  date_created: string,
+  description: string,
+  location: string,
+  title: string,
+  photographer: string
+  nasa_id: number
 }
 interface ILinks {
-    href: string,
+  href: string,
 }
 
 
 const NasaCard: FunctionComponent<NasaCardProps> = ({ data, links }) => {
-   
 
-    const date = new Date(`${data[0].date_created}`).toLocaleDateString();
-    const [likedNasaData, setLikedNasaData] = useState<number[]>([]);
-    const handleLiked = (id: number) => {
-        if (!likedNasaData.includes(id)) {
-            setLikedNasaData([...likedNasaData, id]);
-        } else {
-          const newLikedProducts = likedNasaData.filter(
-            (nasaId) => nasaId !== id
-          );
-          setLikedNasaData(newLikedProducts);
-        }
-      };
-    return (
-        <div className='nasa-data-card'>
-            <h3>{data[0].title}</h3>
-            <span>Photographed by : {data[0].photographer}</span>
-            <img src={links[0].href} alt='nasa' style={{width:'100%'}} />
-            <img
+
+  const date = new Date(`${data[0].date_created}`).toLocaleDateString();
+  const [likedNasaData, setLikedNasaData] = useState<number[]>([]);
+  const handleLiked = (id: number) => {
+    if (!likedNasaData.includes(id)) {
+      setLikedNasaData([...likedNasaData, id]);
+    } else {
+      const newLikedProducts = likedNasaData.filter(
+        (nasaId) => nasaId !== id
+      );
+      setLikedNasaData(newLikedProducts);
+    }
+  };
+  return (
+    <div className='nasa-data-card'>
+      <h5 className='title' style={{ marginLeft: '10px', marginTop: '15px' }}>{data[0].title}</h5>
+      <span className='photo-credit'>Photographed by : <span className='photographer'>{data[0].photographer}</span> </span>
+      <img src={links[0].href} alt='nasa' height='200px' style={{ width: '100%', marginTop: '10px' }} />
+      <img
         src={likedNasaData.includes(data[0].nasa_id) ? redHeartShape : heartShape}
-        className={"liked"}
+        className="liked"
+        style={{ display: 'block', margin: '10px' }}
         alt="liked"
         onClick={() => handleLiked(data[0].nasa_id)}
       />
-            <span>Date Captured {date}</span><br/>
-            <span>Location {data[0].location}</span>
-            <p className='description'>{data[0].description}</p>
-
-
-
-        </div>);
+      <span className='date' style={{ margin: '10px' }}>{date}</span><br />
+      <span className='location' style={{ margin: '10px' }}>{data[0].location}</span>
+      <p className='description' style={{ margin: '10px' }}>{data[0].description}</p>
+    </div>);
 }
 
 export default NasaCard;
